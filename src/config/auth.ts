edfaa221129a8a5490216d01314f67550215c2ad
yonlu <lss.emailbox@gmail.com>
2020-10-03 00:21:02 -0400

    initial commit

---
+++ src/config/auth.ts
@@ -0,0 +1,7 @@
+export default {
+  jwt: {
+    secret:
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE1OTMzODExNzEsImV4cCI6MTU5MzQ2NzU3MSwic3ViIjoiNTcyMjljMjctZDMwMy00OWZlLTliY2QtYTNkNDlkNjg5NWNhIn0.Sub_wCHzy6xtH5BQWVcgTuZvu4AlSfd6UEgblx8nvkg',
+    expiresIn: '1d',
+  },
+};
