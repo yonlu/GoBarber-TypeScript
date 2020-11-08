9399e89f7992f5bb00dc9ab4709ca052a6c6b420
yonlu <lss.emailbox@gmail.com>
2020-11-07 21:46:48 -0500

    test: password recovery email sending

---
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -0,0 +1,43 @@
+import { injectable, inject } from 'tsyringe';
+
+import AppError from '@shared/errors/AppError';
+import IUsersRepository from '../repositories/IUsersRepository';
+import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider';
+import IUserTokensRepository from '../repositories/IUserTokensRepository';
+
+// import User from '../infra/typeorm/entities/User';
+
+interface IRequest {
+  email: string;
+}
+
+@injectable()
+class SendForgotEmailPasswordEmailService {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+
+    @inject('MailProvider')
+    private mailProvider: IMailProvider,
+
+    @inject('UserTokensRepository')
+    private userTokensRepository: IUserTokensRepository,
+  ) {}
+
+  public async execute({ email }: IRequest): Promise<void> {
+    const user = await this.usersRepository.findByEmail(email);
+
+    if (!user) {
+      throw new AppError('User does not exist.');
+    }
+
+    await this.userTokensRepository.generate(user.id);
+
+    this.mailProvider.sendMail(
+      email,
+      'Password recovery request has been received',
+    );
+  }
+}
+
+export default SendForgotEmailPasswordEmailService;
