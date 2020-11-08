76a956a9089193cda2e6f5de121ccf72c7820172
yonlu <lss.emailbox@gmail.com>
2020-11-07 23:04:47 -0500

    test: password reset

--- src/modules/users/repositories/fakes/FakeUserTokensRepository.ts
+++ src/modules/users/repositories/fakes/FakeUserTokensRepository.ts
@@ -1,6 +1,6 @@
 import { uuid } from 'uuidv4';
 
-import IUserTokensRepository from '@modules/users/repositories/IUserTokenRepository';
+import IUserTokensRepository from '@modules/users/repositories/IUserTokensRepository';
 
 import UserToken from '../../infra/typeorm/entities/UserToken';
 
@@ -17,6 +17,14 @@ class FakeUserTokensRepository implements IUserTokensRepository {
     });
 
     this.userTokens.push(userToken);
+
+    return userToken;
+  }
+
+  public async findByToken(token: string): Promise<UserToken | undefined> {
+    const userToken = this.userTokens.find(
+      findToken => findToken.token === token,
+    );
 
     return userToken;
   }
