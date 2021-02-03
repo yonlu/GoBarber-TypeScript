bf688bd9afba5d3e28819fc266548e7a84c29105
yonlu <lss.emailbox@gmail.com>
2021-02-03 14:32:18 -0500

    feat: able to book an appointment

--- src/modules/appointments/services/ListProviderDayAvailabilityService.spec.ts
+++ src/modules/appointments/services/ListProviderDayAvailabilityService.spec.ts
@@ -15,11 +15,13 @@ describe('ListProviderDayAvailability', () => {
   it("should be able to list provider's availability for the day", async () => {
     await fakeAppointmentsRepository.create({
       provider_id: 'user',
+      user_id: 'user',
       date: new Date(2020, 4, 20, 14, 0, 0),
     });
 
     await fakeAppointmentsRepository.create({
       provider_id: 'user',
+      user_id: 'user',
       date: new Date(2020, 4, 20, 15, 0, 0),
     });
 
